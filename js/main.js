fbdfd83968ad23c7976bbc1e39f5759536a477c4
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-15 00:11:22 +0200

    Add html and css

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+function addPurchase() {
+    alert("sdflsdf");
+}
+//     let numb = document.getElementById("money").value;
+//     let rateEur = 1.18;
+//     let rateUan = 0.15;
+//     let rateAzn = 0.61;
+//     var result;
+//     let curr;
+// function
+//     switch(true) {
+//         case document.getElementById("currency_1").checked===true: 
+//             curr = "EUR";
+//             result = numb / rateEur;
+//         break;
+//         case document.getElementById("currency_2").checked===true:
+//             curr = "UAN";
+//             result = numb / rateUan;
+//         break;
+//         case document.getElementById("currency_3").checked===true: 
+//             curr = "AZN";
+//             result = numb / rateAzn;
+//         break;
+//     }
+
+//     result = `You'll get ${result.toFixed(2)} ${curr}`;
+//     document.getElementById("message").innerHTML = result;
+//     document.getElementById("message").style.backgroundColor = "rgba(20,200,200,0.8)";
+//     return result;
+// }
