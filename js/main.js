278194cdde408c629a5c5602141c801d71e6717f
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-18 00:15:48 +0200

    Add all js-function and finish project

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function addPurchase() {
         return; 
     }
 
-    let quantityProduct = document.getElementById("userQuantity").value;
+    let quantityProduct = Number(document.getElementById("userQuantity").value);
     if(quantityProduct == 0) {
         alert("Please, enter the quantity of the product");
         return
@@ -37,10 +37,19 @@ function addPurchase() {
     // LET'S FIND IS THIS NAMEOFPRODUCT EXIST IN OUR ARRAY. IF IT IS NOT EXIST WE PUSH OBJECT TO THE ARRAY
 
     let findSameObj = arr.find(myproduct => myproduct.name === nameProduct);
+    let findIndOfSameObj = arr.findIndex(myproduct => myproduct.name === nameProduct);
+
     if(findSameObj !== undefined) {
-        findSameObj.quantity = parseInt(findSameObj.quantity, 10) + parseInt(quantityProduct, 10);
-        findIndOfSameObj = arr.findIndex(myproduct => myproduct.name === nameProduct);
-        arr[findIndOfSameObj].quantity = findSameObj.quantity;
+        if(findSameObj.purchased === isBuyed) {
+            arr[findIndOfSameObj].quantity = findSameObj.quantity + quantityProduct;
+        }
+        else if(quantityProduct <= findSameObj.quantity) {
+            arr[findIndOfSameObj].quantity = findSameObj.quantity - quantityProduct;
+        }
+        else if(quantityProduct > findSameObj.quantity) {
+            arr[findIndOfSameObj].quantity = quantityProduct - findSameObj.quantity;
+            arr[findIndOfSameObj].purchased = isBuyed;
+        }
     }
     else {
         arr.push(purchase);
@@ -61,11 +70,6 @@ for(let i = 0; i < arr.length; i++) {
 }
 
 function mySort() {
-// lET SORT ARRAY
-    arr.sort(function(a, b) {
-        return a.purchased - b.purchased;
-    });
-
     // LET DELETE REFLECTION OF ARRAY IN HTML IF IT IS EXIST
 
 let myNode = document.getElementById("container");
@@ -73,9 +77,42 @@ while (myNode.firstChild) {
     myNode.removeChild(myNode.firstChild);
 }
 
-// LET DISPLAY OUR NEW ARRAY IN HTML
+// lET SORT ARRAY
+arr.sort(function(a, b) {
+    var purchasedA = a.purchased;
+    var purchasedB = b.purchased;
+
+    if (purchasedA < purchasedB) {
+        return -1;
+      }
+      if (purchasedA > purchasedB) {
+        return 1;
+      }
+      return 0;
+    });
+
+//LET DISPLAY OUR NEW ARRAY IN HTML
 
 for(let i = 0; i < arr.length; i++) {
     container.insertAdjacentHTML('beforeend', `<input type="checkbox" ${arr[i].purchased} id="inContainer_${i}" <label for ="inContainer_${i}">${arr[i].name} - ${arr[i].quantity}</p>`);
+    }
+}
+
+function bought() {
+    let nameBought = prompt("Enter name of the product you want to mark as bought");
+    let findIndOfSameObj = arr.findIndex(a => a.name === nameBought);
+    arr[findIndOfSameObj].purchased = "checked";
+
+// LET DELETE REFLECTION OF ARRAY IN HTML IF IT IS EXIST
+
+    let myNode = document.getElementById("container");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+
+// LET DISPLAY OUR NEW ARRAY IN HTML
+
+    for(let i = 0; i < arr.length; i++) {
+        container.insertAdjacentHTML('beforeend', `<input type="checkbox" ${arr[i].purchased} id="inContainer_${i}" <label for ="inContainer_${i}">${arr[i].name} - ${arr[i].quantity}</p>`);
     }
 }
